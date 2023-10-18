31d5b4020c6f1efa2a6fe039931c062e114b842c
Patrick Millerson <patmillerson@gmail.com>
2023-10-18 08:24:51 -0400

    Update settings.py

--- django_cpanel/settings.py
+++ django_cpanel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vb#wcnsz)(uklud1$d-4e-n%im#x4xzklf!@a$0z6)isq$gzrn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
